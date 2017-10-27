6a0a50b05e403ae7019dcc14bd81a769e9f9063d
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-27 12:10:02 -0700

    Adds methods inside application_controller for user to sign_in, sign_out and identify the current user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(user)
+      #redirect_to main index page
+    else
+      flash.now[:errors] = @user.erros.full_messages
+      #render new
+    end
+  end
+
+  def show
+    #render new
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
