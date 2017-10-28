d38aed2c1f755a748e5807c4b14c2e598d93101f
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-27 17:22:50 -0700

    Adds simple views for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,16 +2,18 @@ class AlbumsController < ApplicationController
 
   def create
     @album = Album.new(album_params)
-    if @alnum.save
+    if @album.save
       redirect_to album_url(@album)
     else
+      @band = @album.band
       flash.now[:errors] = @album.errors.full_messages
       render :new
     end
   end
 
   def new
-    @album = Album.new
+    @band = Band.find(params[:band_id])
+    @album = Album.new(band_id: params[:band_id])
     render :new
   end
 
